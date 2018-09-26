8e966733cfe7ffaf90ce5c3f41a181e64ecf53cd
Pedro Vanzella <pedro@pedrovanzella.com>
2018-09-25 21:43:56 -0300

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="do_the_webp",
-    version="0.1.2",
+    version="0.1.3",
     url="https://github.com/pedrovanzella/do-the-webp",
 
     author="Pedro Vanzella",
