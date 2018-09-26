016957e5fa94b12189cf4d5b09ef15f1ba0e5bdd
Pedro Vanzella <pedro@pedrovanzella.com>
2018-09-25 21:41:03 -0300

    Add compressor module

--- do_the_webp/compressor.py
+++ do_the_webp/compressor.py
@@ -4,6 +4,12 @@ import shutil
 
 from typing import List
 
-def compress(dir: str, path: str=".") -> List[str]:
-    """Compresses the contents of `dir` and outputs a cbz to `path`"""
-    pass
+def compress(src: str, dst: str=".") -> str:
+    """Compresses the contents of `dir` and outputs a cbz to `path`.
+    Returns the filename
+    """
+    basename = os.path.basename(src)
+    with zipfile.ZipFile(os.path.join(dst, basename + '.cbz'), 'a') as myzip:
+        for file in os.listdir(src):
+            myzip.write(os.path.join(src, file))
+    return basename + ".cbz"
--- tests/test_compressor.py
+++ tests/test_compressor.py
@@ -2,21 +2,21 @@ from do_the_webp.compressor import compress
 
 import shutil
 import os
+import zipfile
 
 COMPRESS_TO_DIR='tmp'
+SOURCE_DIR='samples/to_be_compressed'
 
 def setup_module():
     os.mkdir(COMPRESS_TO_DIR)
-    # copy samples/dir
 
 def teardown_module():
     shutil.rmtree(COMPRESS_TO_DIR)
 
 def test_compress_dir():
-    # call compress passing a dir
-    # make sure the zip contains the files in dir
-    # the return value should be a list of files
-    # all files that exist on the dir should be the ones in the zip
-    # no extras
-    # no missing
-    pass
+    zipped = compress(SOURCE_DIR, COMPRESS_TO_DIR)
+    assert zipped == 'to_be_compressed.cbz', "file is properly named"
+    compressed_files = []
+    with zipfile.ZipFile(os.path.join(COMPRESS_TO_DIR, zipped), 'r') as z:
+        compressed_files = [os.path.basename(x) for x in z.namelist()]
+    assert os.listdir(SOURCE_DIR) == compressed_files, "all files in the source dir were zipped"
