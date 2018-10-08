55058822e67984d7178008ed2c602b867c0234d2
Pedro Vanzella <pedro@pedrovanzella.com>
2018-10-08 15:22:19 -0300

    Add traverser module

--- do_the_webp/traverser.py
+++ do_the_webp/traverser.py
@@ -1,6 +1,14 @@
 from typing import List, Tuple
+import glob
+import os
 
 
 def traverse(dir: str) -> List[Tuple[str, str]]:
     """Traverses a directory and returns a list of tuples of paths and files"""
-    return [('', '')]
+    files = []
+    for filename in glob.iglob(dir + '/**/*', recursive=True):
+        if os.path.isfile(filename):
+            dir = os.path.dirname(filename)
+            name = os.path.basename(filename)
+            files.append((dir, name))
+    return files
--- tests/test_traverser.py
+++ tests/test_traverser.py
@@ -1,9 +1,13 @@
 from do_the_webp.traverser import traverse
 
 
-def setup_module():
-    pass
-
-
-def teardown_module():
-    pass
+def test_traverse_tree():
+    assert set(traverse('samples/tree')) == set([
+        ('samples/tree/a', '0'),
+        ('samples/tree/a', '4'),
+        ('samples/tree/b/ba', '1'),
+        ('samples/tree/b/ba', '5'),
+        ('samples/tree/b/bb', '2'),
+        ('samples/tree/b/bb/bba', '6'),
+        ('samples/tree/c', '3'),
+    ]), "Traverses a tree properly"
