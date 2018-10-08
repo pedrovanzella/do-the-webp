500d64928143becb913dcb8a5f298945e15db670
Pedro Vanzella <pedro@pedrovanzella.com>
2018-10-08 15:30:53 -0300

    yield instead of returning the full list

--- do_the_webp/traverser.py
+++ do_the_webp/traverser.py
@@ -1,14 +1,11 @@
-from typing import List, Tuple
 import glob
 import os
 
 
-def traverse(dir: str) -> List[Tuple[str, str]]:
+def traverse(dir: str):
     """Traverses a directory and returns a list of tuples of paths and files"""
-    files = []
     for filename in glob.iglob(dir + '/**/*', recursive=True):
         if os.path.isfile(filename):
             dir = os.path.dirname(filename)
             name = os.path.basename(filename)
-            files.append((dir, name))
-    return files
+            yield (dir, name)
