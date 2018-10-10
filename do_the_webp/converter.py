4ce69eafc26af0507f6a04f6dbf328e47e2395a2
Pedro Vanzella <pedro@pedrovanzella.com>
2018-10-10 09:51:50 -0300

    Version bump

--- do_the_webp/converter.py
+++ do_the_webp/converter.py
@@ -2,14 +2,14 @@ import os
 from PIL import Image
 
 
-VALID_FORMATS = ['.jpg', '.JPG', '.jpeg', '.JPEG']
+VALID_FORMATS = ['jpg', 'JPG', 'jpeg', 'JPEG']
 
 
 def convert(image: str, dst_path: str) -> str:
     """Takes a jpeg image and outputs a webp version of it"""
     basename = os.path.basename(image)
 
-    if os.path.splitext(basename)[1] not in VALID_FORMATS:
+    if os.path.splitext(basename)[1][1:] not in VALID_FORMATS:
         return None
 
     output = os.path.splitext(basename)[0] + '.webp'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="do_the_webp",
-    version="0.2.0",
+    version="0.2.1",
     url="https://github.com/pedrovanzella/do-the-webp",
 
     author="Pedro Vanzella",
